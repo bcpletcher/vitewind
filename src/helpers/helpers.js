48a46f7c56ace94ee9c1756b81b41c1a07acf85b
Benjamin Pletcher <pletcher.accounts@icloud.com>
2021-12-15 19:16:30 -0500

    Changed Helpers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,16 +1,28 @@
 export default {
 	methods: {
 		helpers() {
-			const thing = (arg) => {
-				console.log(arg);
-				return 'test';
-			}
-
 			return {
-				thing
+				thing(arg) {
+					return new Promise(resolve => {
+						console.log(arg);
+						resolve('I am a promise');
+					})
+				},
+				otherThing(arg) {
+					console.log('im not a promise')
+				},
+				finalThing(arg) {
+					console.log('this is just short hand object syntax for methods/functions')
+				}
 			}
 		}
-
 	}
-
 }
+// export default {
+// 	mixins: [helpers],
+// 	methods: {
+// 		async doThingAndThenSomethingElse() {
+// 			let response = await this.helpers.thing("Hi, my name is Thing. I'm a promise, and I live inside helpers.");
+// 			console.log(response);
+// 		}
+// }
